aec3f160ec88d9a5a633de14f62bdb1322ee5a6d
Ishaant Kumar Singh <ishaant97@gmail.com>
2026-02-24 14:50:23 +0530

    feat: replace loading placeholder with Loader component in ProtectedRoutes for improved user experience

--- src/routes/ProtectedRoutes.jsx
+++ src/routes/ProtectedRoutes.jsx
@@ -1,12 +1,13 @@
 import { Navigate, Outlet } from "react-router";
 import { useAuth } from "../context/AuthContext";
+import Loader from "../components/Loader/Loader";
 
 function ProtectedRoutes() {
     // const isLoggedIn = false;
     const { isAuthenticated, loading } = useAuth();
 
     if (loading) {
-        return <div>Loading...</div>
+        return <Loader message="Verifying your session..." />
     }
     return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
 }
